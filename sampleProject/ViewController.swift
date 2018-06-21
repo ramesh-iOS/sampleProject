7dd45cc589944302ecea566f5127530467bd426a
jenkins <jenkins@softprodigys-Mac-mini.local>
2018-06-21 13:53:35 +0530

    initial commit 2

---
+++ sampleProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  sampleProject
+//
+//  Created by jenkins on 20/06/18.
+//  Copyright © 2018 jenkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello every one")
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
